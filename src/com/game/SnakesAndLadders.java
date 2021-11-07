298f1c2c499b0769c386387a4b9e878cc801a5bb
memories2013 <nshivaprasad41@gmail.com>
2021-11-07 22:26:42 +0530

    Single player at starting position 0

--- src/com/game/SnakesAndLadders.java
+++ src/com/game/SnakesAndLadders.java
@@ -5,8 +5,10 @@ public class SnakesAndLadders {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("SnakesAndLadders");
+		int position = 0;
+		System.out.println(" Welcome player!! ");
+		
+		System.out.println(" Current position: " + position);
 	}
 
 }
-

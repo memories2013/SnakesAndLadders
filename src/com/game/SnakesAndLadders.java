ea618a772e8e9c21a777a0173f958bbdec11074a
memories2013 <nshivaprasad41@gmail.com>
2021-11-08 11:48:24 +0530

    2nd Player Is Introduced And Reporting Who Is The Winner!

--- src/com/game/SnakesAndLadders.java
+++ src/com/game/SnakesAndLadders.java
@@ -6,17 +6,26 @@ public class SnakesAndLadders {
 		// TODO Auto-generated method stub
 		
 		int position = 0;
+		int position2 = 0;
 		System.out.println(" Welcome player!! ");
 		
 		
 		int dice;
 		int option;
 		int roll = 0;
+		int turn = 0;
+		int oldTurn = 0;
 		
 		
-		while (position < 100) {
+		while (position < 100 && position2 < 100) {
+			
+			if (turn == 0) 
+				System.out.println(" Player 1 is playing");
+			else
+				System.out.println(" Player 2 is playing");
+			
 			dice = (int)Math.floor( (Math.random() * 10) % 6 + 1); 
-			System.out.println("\n The dice was rolled to: " + dice);
+			System.out.println(" The dice was rolled to: " + dice);
 			option = (int)Math.floor(Math.random() * 3); 
 			
 			switch(option) {
@@ -25,25 +34,56 @@ public class SnakesAndLadders {
 				break;
 			case 1: 
 				System.out.println(" Player action: ladder");
-				position += dice;
+				if (turn == 0) 
+					position += dice;
+					
+				else
+					position2 += dice;
+				
 				if (position > 100) {
 					position -= dice;
-					System.out.println(" Exceeded 100 so not counting");
+					System.out.println(" Player1 exceeded 100 so not counting");
+				}
+				
+				if (position2 > 100) {
+					position2 -= dice;
+					System.out.println(" Player2 exceeded 100 so not counting");
 				}
 				break;
 			case 2: 
 				System.out.println(" Player action: snake");
-				position -= dice;
-				position = position < 0 ? 0 : position;
+				if (turn != 0) 
+					position -= dice;
+					
+				else
+					position2 -= dice;
+				
+				
+				if (position < 0) {
+					position = 0;
+					System.out.println(" Player1 reset to 0");
+				}
+				
+				if (position2 < 0) {
+					position2 = 0;
+					System.out.println(" Player2 reset to 0");
+				}
+				
+				turn = (turn + 1) % 2;
 				break;
 							
 			}
 			
-			System.out.println(" Current position: " + position);
+			if (oldTurn == 0)
+				System.out.println(" Current position for Player1: " + position + "\n");
+			else
+				System.out.println(" Current position for Player2: " + position2 + "\n");
 			roll++;
+			oldTurn = turn;
 		}
 		
-		System.out.println(" Congratulations!! You took " + roll + " rolls to win the game!!");
+		System.out.println(" Congratulations Player" + (turn + 1) + "!!");
+		System.out.println(" We took " + roll + " rolls to win the game!!");
 	}
 
 }
